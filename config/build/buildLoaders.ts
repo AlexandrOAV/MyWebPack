dd60391cf0c07d6377b86d6765b9fcf3e3caa981
Opanasiuk Olexandr <121792730+AlexandrOAV@users.noreply.github.com>
2024-03-13 14:02:47 +0200

    add babel loader

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -72,14 +72,27 @@ const cssLoaderWithModules = {
         ]
       }
         
-      
+const babelLoader = {
+    test: /\.tsx?$/,
+    exclude: /node_modules/,
+    use: {
+      loader: "babel-loader",
+      options: {
+        presets: ['@babel/preset-env', 
+                  "@babel/preset-typescript", 
+                  ["@babel/preset-react",  {runtime: isDev ? "automatic" : "classic"}]
+        ]
+      }
+    }
+  }     
 
 
 return [
     assetLoader, 
     scssLoader,
-    tsLoader,
-    svgrLoader
+    // tsLoader,
+    svgrLoader,
+    babelLoader
 ]
 
 //ts-loader вміє працювати з LSX // Упорядкування важливе//як би не використовувася TS, то потрібен був би babel-loader 
